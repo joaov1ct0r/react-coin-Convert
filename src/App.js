40fd91f80e1a3fb2aff356e9ea3cfffda4f5ff33
Joao Victor <jov9051@outlook.com>
2022-01-20 20:31:01 -0300

    style applied to div conversor-container

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
     return (
         <div className="App">
             <Conversor MoedaA={'USD'} MoedaB={'BRL'}></Conversor>
+            <Conversor MoedaA={'BRL'} MoedaB={'USD'}></Conversor>
+            <Conversor MoedaA={'USD'} MoedaB={'EUR'}></Conversor>
+            <Conversor MoedaA={'EUR'} MoedaB={'USD'}></Conversor>
+            <Conversor MoedaA={'BRL'} MoedaB={'EUR'}></Conversor>
+            <Conversor MoedaA={'EUR'} MoedaB={'BRL'}></Conversor>
         </div>
     );
 }
