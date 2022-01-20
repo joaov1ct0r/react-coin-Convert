7b1ff3337d1ed0c0eb10f434c22bb93c18bef817
Joao Victor <jov9051@outlook.com>
2022-01-20 19:33:13 -0300

    div usd-brl-container code created with 1 h1 1 input 1 button and 1 h2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div className="usd-brl-container"></div>
+            <div className="usd-brl-container">
+                <h1>USD - BRL</h1>
+                <input type="text" name="usd" placeholder="USD:" />
+                <button>Converter</button>
+                <h2>Valor Convertido:</h2>
+            </div>
         </div>
     );
 }
