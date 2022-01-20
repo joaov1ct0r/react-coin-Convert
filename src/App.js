40bcd8910e68929e3f56e747e12a7e149fac945d
Joao Victor <jov9051@outlook.com>
2022-01-20 19:38:51 -0300

    function handleValue created inside app.js file setting new value to UsdValue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    let [UsdValue, setUsdValue] = useState('');
+
+    function handleValue(event) {
+        setUsdValue(event.target.value);
+    }
     return (
         <div className="App">
             <div className="usd-brl-container">
                 <h1>USD - BRL</h1>
-                <input type="text" name="usd" placeholder="USD:" />
+                <input
+                    onChange={handleValue}
+                    type="text"
+                    name="usd"
+                    placeholder="USD:"
+                />
                 <button>Converter</button>
-                <h2>Valor Convertido:</h2>
+                <h2>Valor Convertido: {UsdValue}</h2>
             </div>
         </div>
     );
