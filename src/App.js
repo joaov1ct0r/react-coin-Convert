be69bb3442ae85f7dd0bf1714ce0b4530aaa1718
Joao Victor <jov9051@outlook.com>
2022-01-20 20:32:41 -0300

    div linha creatd and first row components added to it

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+            <div className="linha"></div>
             <Conversor MoedaA={'USD'} MoedaB={'BRL'}></Conversor>
             <Conversor MoedaA={'BRL'} MoedaB={'USD'}></Conversor>
+            <div className="linha"></div>
             <Conversor MoedaA={'USD'} MoedaB={'EUR'}></Conversor>
             <Conversor MoedaA={'EUR'} MoedaB={'USD'}></Conversor>
-            <Conversor MoedaA={'BRL'} MoedaB={'EUR'}></Conversor>
-            <Conversor MoedaA={'EUR'} MoedaB={'BRL'}></Conversor>
+            <div className="linha">
+                <Conversor MoedaA={'BRL'} MoedaB={'EUR'}></Conversor>
+                <Conversor MoedaA={'EUR'} MoedaB={'BRL'}></Conversor>
+            </div>
         </div>
     );
 }
